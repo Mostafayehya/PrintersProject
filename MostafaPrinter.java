789ed73db1b4115d8821c2571fa32a0596f54461
MostafaYahia <mostafayehyax23@gmail.com>
2020-12-18 16:55:08 +0200

    Change mostafa print

--- MostafaPrinter.java
+++ MostafaPrinter.java
@@ -5,6 +5,10 @@ public class MostafaPrinter{
 		
 		System.out.println("change #1 from mostafa");
 		System.out.println("change #2 from mostafa");
+		System.out.println("change #3 from mostafa");
+		System.out.println("change #4 from mostafa");
+		System.out.println("change #5 from mostafa");
+		System.out.println("change #6 from mostafa");
 	}
 	
 }
